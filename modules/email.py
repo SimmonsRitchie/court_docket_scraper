e367becf73644ec46b1adc354c1987a0957ae036
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-04-11 09:07:47 -0400

    generated email has different intro text and subject line if only one county is chosen for scrape

--- modules/email.py
+++ modules/email.py
@@ -12,7 +12,8 @@ from modules import misc
 
 
 
-def email_notification(base_folder_email, destination_email_addresses, my_email_login, date_and_time_of_scrape, desired_scrape_date_literal):
+def email_notification(base_folder_email, destination_email_addresses, my_email_login,
+                       date_and_time_of_scrape, desired_scrape_date_literal, county_list):
 
     print("Sending email...")
 
@@ -24,10 +25,11 @@ def email_notification(base_folder_email, destination_email_addresses, my_email_
     yesterday_date = (datetime.now() - timedelta(1)).strftime("%a, %b %-d %Y")
 
     # GENERATE EMAIL HTML
-    message = create_final_email_payload(base_folder_email, desired_scrape_date_literal, formatted_date, formatted_time, yesterday_date)
+    message = create_final_email_payload(base_folder_email, desired_scrape_date_literal, formatted_date,
+                                         formatted_time, yesterday_date, county_list)
 
     # GENERATE SUBJECT LINE
-    subject = create_subject_line(base_folder_email, desired_scrape_date_literal, formatted_date, yesterday_date)
+    subject = create_subject_line(base_folder_email, desired_scrape_date_literal, formatted_date, yesterday_date, county_list)
 
     # ACCESS GMAIL AND SEND
     fromaddr = "dsr.newsalert@gmail.com"
@@ -46,7 +48,9 @@ def email_notification(base_folder_email, destination_email_addresses, my_email_
     print("Email sent!")
 
 
-def create_final_email_payload(base_folder_email, desired_scrape_date_literal, formatted_date, formatted_time, yesterday_date):
+def create_final_email_payload(base_folder_email, desired_scrape_date_literal, formatted_date, formatted_time,
+                               yesterday_date, county_list):
+
     print("Generating final HTML payload")
 
     # GENERATE HTML HEAD AND BODY TOP
@@ -60,7 +64,10 @@ def create_final_email_payload(base_folder_email, desired_scrape_date_literal, f
     with open("email_template/mobile_tease_top.html", "r") as fin:
         mobile_tease_top = fin.read()
     # mobile tease content
-    mobile_tease_content = "Here are the latest criminal cases filed in central Pa. courts."
+    if len(county_list) == 1:
+        mobile_tease_content = "Here are the latest criminal cases filed in {} County".format(county_list[0])
+    else:
+        mobile_tease_content = "Here are the latest criminal cases filed in central Pa. courts."
     # mobile tease bottom + start of main email container div
     with open("email_template/mobile_tease_bottom.html", "r") as fin:
         mobile_tease_bottom = fin.read()
@@ -82,15 +89,25 @@ def create_final_email_payload(base_folder_email, desired_scrape_date_literal, f
     with open("email_template/intro_top.html", "r") as fin:
         intro_container_top = fin.read()
     # intro header and contents
-    if desired_scrape_date_literal == "today":
-        intro_header = '<span class="subheading">afternoon scrape</span>'
-        intro_contents = "<p>The following criminal cases were filed in district courts today as of {}.</p>\
-                         <p>Check tomorrow morning's email to see all cases filed today.</p>".format(formatted_time)
-    elif desired_scrape_date_literal == "yesterday":
-        intro_header = '<span class="subheading">Morning scrape</span>'
-        intro_contents = "<p>The following criminal cases were filed in district courts yesterday ({}).</p>\
-                    <p>You can also view a searchable list of these cases\
-                    <a href='https://s3.amazonaws.com/court-dockets/index.html'>here</a>.</p>\
+    # we provide different intros if only one county was chosen for scrape
+    if len(county_list) == 1:
+        intro_header = '<span class="subheading">{} county scrape</span>'.format(county_list[0])
+        if desired_scrape_date_literal == "today":
+            intro_contents = "<p>The following criminal cases were filed in {} County today as of {}.</p>\
+                             <p>Check tomorrow morning's email to see all cases filed today.</p>".format(county_list[0],formatted_time)
+        elif desired_scrape_date_literal == "yesterday":
+            intro_contents = "<p>The following criminal cases were filed in {} County yesterday ({}).</p>\
+                            ".format(county_list[0], yesterday_date)
+    else:
+        if desired_scrape_date_literal == "today":
+            intro_header = '<span class="subheading">afternoon scrape</span>'
+            intro_contents = "<p>The following criminal cases were filed in district courts today as of {}.</p>\
+                             <p>Check tomorrow morning's email to see all cases filed today.</p>".format(formatted_time)
+        elif desired_scrape_date_literal == "yesterday":
+            intro_header = '<span class="subheading">Morning scrape</span>'
+            intro_contents = "<p>The following criminal cases were filed in district courts yesterday ({}).</p>\
+                        <p>You can also view a searchable list of these cases\
+                        <a href='https://s3.amazonaws.com/court-dockets/index.html'>here</a>.</p>\
                     ".format(yesterday_date)
     # intro bottom
     intro_container_bottom = '</div></td></tr>'
@@ -138,15 +155,21 @@ def create_final_email_payload(base_folder_email, desired_scrape_date_literal, f
 
 
 
-def create_subject_line(base_folder_email, desired_scrape_date_literal, formatted_date, yesterday_date):
+def create_subject_line(base_folder_email, desired_scrape_date_literal, formatted_date, yesterday_date, county_list):
 
     print("Creating subject line")
 
     # SET SUBJECT LINE
-    if desired_scrape_date_literal == "today":
-        subject = "Criminal cases filed so far today - {}".format(formatted_date)
-    elif desired_scrape_date_literal == "yesterday":
-        subject = "Criminal cases filed yesterday - {}".format(yesterday_date)
+    if len(county_list) == 1:
+        if desired_scrape_date_literal == "today":
+            subject = "Criminal cases filed in {} County today - {}".format(county_list[0], formatted_date)
+        elif desired_scrape_date_literal == "yesterday":
+            subject = "Criminal cases filed in {} County yesterday - {}".format(county_list[0], yesterday_date)
+    else:
+        if desired_scrape_date_literal == "today":
+            subject = "Criminal cases filed so far today - {}".format(formatted_date)
+        elif desired_scrape_date_literal == "yesterday":
+            subject = "Criminal cases filed yesterday - {}".format(yesterday_date)
 
     # APPEND HOMICIDE NOTE IF NEEDED
     # Access email body
