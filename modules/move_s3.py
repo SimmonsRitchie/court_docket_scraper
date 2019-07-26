685198a38528ee04c5ae47ce1a3eba0a0b0131cd
Daniel SR <daniel.simmons.ritchie@gmail.com>
2019-07-26 15:25:20 -0400

    added error handling to move_s3 so program doesn't crash if something goes wrong

--- modules/move_s3.py
+++ modules/move_s3.py
@@ -8,27 +8,36 @@ import os
 
 # project modules
 from locations import paths
+from modules.email import email_error_notification
 
 def copy_file_to_s3_bucket():
 
-    # GET ENV VARS
-    bucket_name = os.environ.get("BUCKET_NAME")
-    keyID = os.environ.get("KEY_ID")
-    sKeyID = os.environ.get("SECRET_KEY_ID")
-    source_path = str(paths["payload_json"].resolve())
-    destination_path = os.environ.get("DESTINATION_PATH")
+    try:
+        # GET ENV VARS
+        bucket_name = os.environ.get("BUCKET_NAME")
+        keyID = os.environ.get("KEY_ID")
+        sKeyID = os.environ.get("SECRET_KEY_ID")
+        source_path = str(paths["payload_json"].resolve())
+        destination_path = os.environ.get("DESTINATION_PATH")
 
-    # LOGGING
-    logging.info(f"Moving {source_path} to S3 bucker {bucket_name}...")
-    logging.info(f"File will be saved in: {destination_path}")
+        # LOGGING
+        logging.info(f"Moving {source_path} to S3 bucket {bucket_name}...")
+        logging.info(f"File will be saved in: {destination_path}")
 
-    # CONNECT TO S3
-    session = boto3.Session(
-        aws_access_key_id=keyID,
-        aws_secret_access_key=sKeyID,
-    )
-    s3 = session.resource('s3')
+        # CONNECT TO S3
+        session = boto3.Session(
+            aws_access_key_id=keyID,
+            aws_secret_access_key=sKeyID,
+        )
+        s3 = session.resource('s3')
 
-    # UPLOAD
-    s3.Bucket(bucket_name).upload_file(source_path, destination_path)
-    logging.info(f"file uploaded to {destination_path}")
+        # UPLOAD
+        s3.Bucket(bucket_name).upload_file(source_path, destination_path)
+        logging.info(f"file uploaded to {destination_path}")
+    except Exception as e:
+        error_summary = "Someething went wrong when attempting to copy file" \
+                        " to S3 bucket"
+        logging.exception(e)
+        email_error_notification(error_summary, e)
+        return
+
